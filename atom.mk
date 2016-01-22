8dae87acd82ad9b7719b4b81ee4ff14be6e4e2cc
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-01-22 17:24:33 +0100

    [BUILD] Do not use autotools anymore, just alchemy build
    Change-Id: Ief45603fefdb603d41987fbecc10662c0cf33850
    Reviewed-on: https://gerrit.parrot.biz/28572
    Reviewed-by: Nicolas Christe <nicolas.christe@parrot.com>

--- atom.mk
+++ atom.mk
@@ -2,49 +2,34 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CATEGORY_PATH := dragon/libs
 LOCAL_MODULE := libARNetwork
 LOCAL_DESCRIPTION := ARSDK Network Control Library
+LOCAL_CATEGORY_PATH := dragon/libs
 
-LOCAL_LIBRARIES := ARSDKBuildUtils libARSAL libARNetworkAL
-LOCAL_EXPORT_LDLIBS := -larnetwork
+LOCAL_MODULE_FILENAME := libarnetwork.so
+
+LOCAL_LIBRARIES := libARSAL libARNetworkAL
 
 LOCAL_CONFIG_FILES := Build/Config.in
 $(call load-config)
 
-# Copy in build dir so bootstrap files are generated in build dir
-LOCAL_AUTOTOOLS_COPY_TO_BUILD_DIR := 1
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/Includes \
+	$(LOCAL_PATH)/Sources
 
-# Configure script is not at the root
-LOCAL_AUTOTOOLS_CONFIGURE_SCRIPT := Build/configure
+LOCAL_SRC_FILES := \
+	Sources/ARNETWORK_IOBuffer.c \
+	Sources/ARNETWORK_IOBufferParam.c \
+	Sources/ARNETWORK_Manager.c \
+	Sources/ARNETWORK_Receiver.c \
+	Sources/ARNETWORK_RingBuffer.c \
+	Sources/ARNETWORK_Sender.c \
+	gen/Sources/ARNETWORK_Error.c
 
-#Autotools variables
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
-	--with-libARSALInstallDir="" \
-	--with-libARNetworkALInstallDir=""
+LOCAL_INSTALL_HEADERS := \
+	Includes/libARNetwork/ARNetwork.h:usr/include/libARNetwork/ \
+	Includes/libARNetwork/ARNETWORK_Error.h:usr/include/libARNetwork/ \
+	Includes/libARNetwork/ARNETWORK_IOBufferParam.h:usr/include/libARNetwork/ \
+	Includes/libARNetwork/ARNETWORK_Manager.h:usr/include/libARNetwork/
 
-ifeq ("$(TARGET_OS_FLAVOUR)","android")
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--disable-static \
-	--enable-shared \
-	--disable-so-version
-
-else ifneq ($(filter iphoneos iphonesimulator, $(TARGET_OS_FLAVOUR)),)
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--enable-static \
-	--disable-shared \
-	OBJCFLAGS=" -x objective-c -fobjc-arc -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJC="$(TARGET_CC)" \
-	CFLAGS=" -std=gnu99 -x c $(TARGET_GLOBAL_CFLAGS)"
-
-endif
-
-# User define command to be launch before configure step.
-# Generates files used by configure
-define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
-	$(Q) cd $(PRIVATE_SRC_DIR)/Build && ./bootstrap
-endef
-
-include $(BUILD_AUTOTOOLS)
+include $(BUILD_LIBRARY)
