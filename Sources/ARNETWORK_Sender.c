isabella232/libARNetwork

commit 99b62f575ed92e5f9ad2b8383a93964d58c7288e

[COVERITY 10904]
Change-Id: Ie21be011a093f892a1e52344c7757b5b4380ec21

Sources/ARNETWORK_Sender.c
@@ -333,7 +333,13 @@ void* ARNETWORK_Sender_ThreadRun (void* data)
                     senderPtr->lastPingValue = -1;
                 }
                 inputBufferPtrTemp = senderPtr->inputBufferPtrMap[ARNETWORK_MANAGER_INTERNAL_BUFFER_ID_PING];
-                ARNETWORK_IOBuffer_Lock (inputBufferPtrTemp);
+                error = ARNETWORK_IOBuffer_Lock (inputBufferPtrTemp);
+                if (error == ARNETWORK_OK) {
+                    ARNETWORK_IOBuffer_AddData (inputBufferPtrTemp, (uint8_t *)&now, sizeof (now), NULL, NULL, 1);
+                    ARNETWORK_IOBuffer_Unlock (inputBufferPtrTemp);
+                } else {
+                    ARSAL_PRINT (ARSAL_PRINT_ERROR, ARNETWORK_SENDER_TAG, "ARNETWORK_IOBuffer_Lock() failed; error: %s", ARNETWORK_Error_ToString (error));
+                }
                 ARNETWORK_IOBuffer_AddData (inputBufferPtrTemp, (uint8_t *)&now, sizeof (now), NULL, NULL, 1);
                 ARNETWORK_IOBuffer_Unlock (inputBufferPtrTemp);
                 senderPtr->pingStartTime.tv_sec = now.tv_sec;

commit 662f232b560e07dbc73cbe9d1e64d414e1dfc48f

[DEBUG] fix regression
introduced by commit 99b62f575ed92e5f9ad2b8383a93964d58c7288e

Change-Id: Ic36124b24e37691103ed026360167ed7b1b48b25
Reviewed-on: https://gerrit.parrot.biz/56565
Reviewed-by: Maxime Maitre <maxime.maitre@parrot.com>

Sources/ARNETWORK_Sender.c
@@ -340,8 +340,6 @@ void* ARNETWORK_Sender_ThreadRun (void* data)
                 } else {
                     ARSAL_PRINT (ARSAL_PRINT_ERROR, ARNETWORK_SENDER_TAG, "ARNETWORK_IOBuffer_Lock() failed; error: %s", ARNETWORK_Error_ToString (error));
                 }
-                ARNETWORK_IOBuffer_AddData (inputBufferPtrTemp, (uint8_t *)&now, sizeof (now), NULL, NULL, 1);
-                ARNETWORK_IOBuffer_Unlock (inputBufferPtrTemp);
                 senderPtr->pingStartTime.tv_sec = now.tv_sec;
                 senderPtr->pingStartTime.tv_nsec = now.tv_nsec;
                 senderPtr->isPingRunning = 1;